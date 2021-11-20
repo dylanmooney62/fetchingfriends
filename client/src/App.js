587a5bd5cb22b4b8770e08a7f8ec2ddb40a5637b
Dylan Mooney <33304160+dylanmooney62@users.noreply.github.com>
2021-11-20 16:18:28 +0000

    added airbnb eslint

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,5 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-const App = () => {
-  const [submissions, setSubmissions] = useState([]);
-
-  useEffect(() => {
-    const fetchSubmissions = async () => {
-      try {
-        const { data } = await axios.get('/api/v1/submissions');
-
-        setSubmissions(data?.submissions || []);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchSubmissions();
-  }, []);
-
-  if (!submissions) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <ul
-      style={{
-        listStyle: 'none',
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 1fr)',
-      }}
-    >
-      <button className="btn btn-primary">Hello</button>
-
-      {submissions.map(({ _id, title, description, imgUrl }) => (
-        <li key={_id}>
-          <div>
-            <img src={imgUrl} alt={description} style={{ width: 300 }} />
-            <h2>{title}</h2>
-            <p>{description}</p>
-          </div>
-        </li>
-      ))}
-    </ul>
-  );
-};
+const App = () => <div>Hello World</div>;
 
 export default App;
